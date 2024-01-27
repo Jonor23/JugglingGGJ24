a326d084e0eb3d7f6f8c1b91d28b9db0596e8ae9
SevenTerabyte <91615240+SevenTerabyte@users.noreply.github.com>
2024-01-27 17:49:12 +0000

    Keep

--- Assets/Scripts/All_Items.cs
+++ Assets/Scripts/All_Items.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,6 +13,13 @@ public class All_Items : MonoBehaviour
     private GameObject Bowing_Ball_1;
     private GameObject Bowing_Ball_2;
     private GameObject Bowing_Ball_3;
+
+    private GameObject jelly;
+    private GameObject paper_Plane;
+    private GameObject real_Chicken;
+    private GameObject squeaky_Chicken;
+    private GameObject triangle;
+    private GameObject triangle_Beater;
 
 
     public static All_Items Instance;
@@ -35,23 +41,35 @@ public class All_Items : MonoBehaviour
         Bowing_Ball_2 = Resources.Load<GameObject>("Bowing_Ball_2");
         Bowing_Ball_3 = Resources.Load<GameObject>("Bowing_Ball_3");
 
-
+        jelly = Resources.Load<GameObject>("Jelly");
+        paper_Plane = Resources.Load<GameObject>("Paper_Plane");
+        real_Chicken = Resources.Load<GameObject>("Real_Chicken");
+        squeaky_Chicken = Resources.Load<GameObject>("Squeaky_Chicken");
+        triangle = Resources.Load<GameObject>("Triangle");
+        triangle_Beater = Resources.Load<GameObject>("Triangle_Beater");
 
 
         itemList.Add(new Juggling_Item("juggling_Pin_Blue", juggling_Pin_Blue, 0.0003f));
         itemList.Add(new Juggling_Item("juggling_Pin_Green", juggling_Pin_Green, 0.0003f));
         itemList.Add(new Juggling_Item("juggling_Pin_Red", juggling_Pin_Red, 0.0003f));
-        itemList.Add(new Juggling_Item("Juggling_Ball", juggling_Ball, 0.00026f));
+        itemList.Add(new Juggling_Item("Juggling_Ball", juggling_Ball, 0.00025f));
         itemList.Add(new Juggling_Item("Bowing_Ball_1", Bowing_Ball_1, 0.0005f));
         itemList.Add(new Juggling_Item("Bowing_Ball_2", Bowing_Ball_2, 0.0005f));
         itemList.Add(new Juggling_Item("Bowing_Ball_3", Bowing_Ball_3, 0.0005f));
+
+        itemList.Add(new Juggling_Item("jelly", jelly, 0.0003f));
+        itemList.Add(new Juggling_Item("paper_Plane", paper_Plane, 0.0003f));
+        itemList.Add(new Juggling_Item("real_Chicken", real_Chicken, 0.0001f));
+        itemList.Add(new Juggling_Item("squeaky_Chicken", squeaky_Chicken, 0.0003f));
+        itemList.Add(new Juggling_Item("triangle", triangle, 0.0004f));
+        itemList.Add(new Juggling_Item("triangle_Beater", triangle_Beater, 0.0003f));
     }
 
     public Juggling_Item FindItemByName(string name)
     {
-        for(int i = 0; i < itemList.Count; i++)
-        {            
-            if(itemList[i].ItemName == name)
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            if (itemList[i].ItemName == name)
             {
                 return itemList[i];
             }
