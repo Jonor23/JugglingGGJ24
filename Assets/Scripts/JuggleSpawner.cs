737ac6f2ac1dd85bb78aa0b6573deb9d68fe6765
SevenTerabyte <91615240+SevenTerabyte@users.noreply.github.com>
2024-01-27 13:21:09 +0000

    Again
    Yes

--- Assets/Scripts/JuggleSpawner.cs
+++ Assets/Scripts/JuggleSpawner.cs
@@ -7,14 +7,19 @@ public class JuggleSpawner : MonoBehaviour
     private Transform a_Transform;
     private Transform spawner_Transform;
 
-    private GameObject juggling_Item;
+    private GameObject juggling_Pin_Blue;
+    private GameObject juggling_Pin_Green;
+    private GameObject juggling_Pin_Red;
 
     void Start()
     {
         a_Transform = gameObject.transform;
         spawner_Transform = a_Transform.Find("Spawner");
 
-        juggling_Item = Resources.Load<GameObject>("Juggling_Pin_Blue");
+        juggling_Pin_Blue = Resources.Load<GameObject>("Juggling_Pin_Blue");
+        juggling_Pin_Green = Resources.Load<GameObject>("Juggling_Pin_Green");
+        juggling_Pin_Red = Resources.Load<GameObject>("Juggling_Pin_Red");
+
 
         StartCoroutine("SpawnerMovement");
     }
@@ -33,16 +38,39 @@ public class JuggleSpawner : MonoBehaviour
         {
             spawner_Transform.Translate(new Vector2(100, 0));
             yield return new WaitForSeconds(2.0f);
-            Spawn();
+            
+            switch (Random.Range(0, 3))
+            {
+                case 0:
+                    Spawn(juggling_Pin_Blue);
+                    break;
+                case 1:
+                    Spawn(juggling_Pin_Green);
+                    break;
+                case 2:
+                    Spawn(juggling_Pin_Red);
+                    break;
+                default:
+                    break;
+
+            }
             yield return new WaitForSeconds(3.0f);
         }       
     }
 
     /// <summary>
-    /// The function to Spawn new item on Call
+    /// The function Spawn a blue juggling pin on Call
     /// </summary>
     private void Spawn()
     {
-        Instantiate(juggling_Item, spawner_Transform.position, Quaternion.identity, a_Transform);
+        Instantiate(juggling_Pin_Blue, spawner_Transform.position, Quaternion.identity, a_Transform);
+    }
+
+    /// <summary>
+    /// The function Spawn a specific item on Call
+    /// </summary>
+    private void Spawn(GameObject Object)
+    {
+        Instantiate(Object, spawner_Transform.position, Quaternion.identity, a_Transform);
     }
 }
