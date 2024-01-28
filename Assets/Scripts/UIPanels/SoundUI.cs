b61b41d8e336c1ca843e738a1e28e59b17fe66ac
William <infernox1099@gmail.com>
2024-01-28 15:12:12 +0000

    Maybe a finished product?

--- Assets/Scripts/UIPanels/SoundUI.cs
+++ Assets/Scripts/UIPanels/SoundUI.cs
@@ -7,9 +7,23 @@ public class SoundUI : MonoBehaviour
 
     [SerializeField]
     private AK.Wwise.Event sound = null;
+    [SerializeField]
+    private AK.Wwise.Event sound2 = null;
+    [SerializeField]
+    private AK.Wwise.Switch mySwitch;
+
+    private void Start()
+    {
+        sound2.Post(this.gameObject);
+    }
 
     public void click()
     {
         sound.Post(this.gameObject);
+    }
+
+    public void swap()
+    {
+        mySwitch.SetValue(this.gameObject);
     }
 }
