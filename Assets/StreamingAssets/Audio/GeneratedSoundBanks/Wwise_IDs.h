8f491dffc0bccf54136acf05defc45a84bc831a7
Jono Richards <115578931+Jonor23@users.noreply.github.com>
2024-01-28 12:13:55 +0000

    UI Added and banks built

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BIRDCOLLISION = 239793012U;
+        static const AkUniqueID BOWLINGBALLCOLLISION = 1944248036U;
+        static const AkUniqueID FROGCOLLISION = 3433417529U;
+        static const AkUniqueID HANDCOLLISION = 3860318738U;
+        static const AkUniqueID JELLYCOLLISION = 2980418053U;
+        static const AkUniqueID JUGGLINGPINCOLLISION = 1538471339U;
+        static const AkUniqueID PAPERPLANECOLLISION = 3904978471U;
+        static const AkUniqueID REALCHICKENCOLLISION = 826343242U;
+        static const AkUniqueID ROCKETCOLLISION = 1684680715U;
+        static const AkUniqueID RUBBERCHICKENCOLLISION = 3369665588U;
+        static const AkUniqueID TRIANGLECOLLISION = 2124973191U;
+        static const AkUniqueID UI_INTERACTION = 375339906U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace AMB_SWITCHGROUP
+        {
+            static const AkUniqueID GROUP = 2502726051U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID OUTDOORSCENE_SW = 1293687882U;
+                static const AkUniqueID SALOON_SW = 1358710794U;
+            } // namespace SWITCH
+        } // namespace AMB_SWITCHGROUP
+
+        namespace OBJECT_SWITCHGROUP
+        {
+            static const AkUniqueID GROUP = 2071913860U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BIRD_SW = 3533229697U;
+                static const AkUniqueID BOWLINGBALL_SW = 3236973553U;
+                static const AkUniqueID FROG_SW = 1848499500U;
+                static const AkUniqueID JELLY_SW = 16724608U;
+                static const AkUniqueID JUGGLINGPIN_SW = 169923998U;
+                static const AkUniqueID PAPERPLANE_SW = 2766995314U;
+                static const AkUniqueID REALCHICKEN_SW = 1227152183U;
+                static const AkUniqueID ROCKET_SW = 3656468158U;
+                static const AkUniqueID RUBBERCHICKEN_SW = 3472729409U;
+                static const AkUniqueID TRIANGLE_SW = 553221778U;
+            } // namespace SWITCH
+        } // namespace OBJECT_SWITCHGROUP
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID AMB_BLEND_RTPC = 638808235U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID ROOMVERB = 1572913279U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
